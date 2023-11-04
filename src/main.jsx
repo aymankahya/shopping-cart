931f212cf6bccd6ac428a67ac21cb78fa507e351
Ayman Kahya <kahya.ayman@gmail.com>
2023-11-04 14:49:49 +0100

    Setup styled-components Added styled-components version of Normalize.css reset

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GlobalStyle } from "./setup/setupStyles.js";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
